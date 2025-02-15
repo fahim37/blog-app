eec45bfc3061d39c727d82cf8d22de3222fce5f6
Fahim <ahmed.fahim37@gmail.com>
2025-02-15 18:32:35 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,7 @@
 import ArticlesGrid from "@/components/articles-grid";
-import ContactForm from "@/components/contact-form";
 import Hero from "@/components/hero";
 import PopularCarousel from "@/components/homepage/popular-carousel";
 import WhoAreWe from "@/components/homepage/who-are-we";
-import PopularSidebar from "@/components/popular-sidebar";
-import BlogSidebar from "@/components/side/sidebar";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
